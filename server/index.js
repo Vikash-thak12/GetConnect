a8dc8b4ac09a381c56eadca84c007f639aea0dc5
Vikash Thakur <thakvikash622@gmail.com>
2024-09-05 22:08:08 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,14 @@
+import express from 'express'
+const app = express(); 
+
+
+const port = 3000; 
+
+app.use((req, res) => {
+    res.send("Hello World")
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on PORT: ${port}`);
+    
+})
