84bf0e7af858d08912bac17d481234b3adf93147
Vikash Thakur <thakvikash622@gmail.com>
2024-09-25 19:30:48 +0530

    Customized: For database connection

--- server/index.js
+++ server/index.js
@@ -15,18 +15,18 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 dotenv.config();
 
-const port = process.env.PORT || 3001 
-const app = express(); 
+const port = process.env.PORT || 3001
+const app = express();
 
 
 // Middlewares 
 app.use(express.json())
 app.use(bodyParser.json())
 app.use(helmet())
-app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin"}))
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }))
 app.use(morgan("common"))
-app.use(bodyParser.json({ limit: "30mb", extended: true}))
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}))
+app.use(bodyParser.json({ limit: "30mb", extended: true }))
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
 app.use(cors())
 app.use("/assets", express.static(path.join(__dirname, "public/assets")))   // will set the directory where we are going to store our images  
 
@@ -34,10 +34,10 @@ app.use("/assets", express.static(path.join(__dirname, "public/assets")))   // w
 
 // File Storage 
 const storage = multer.diskStorage({
-    destination: function(req, file, cb) {
+    destination: function (req, file, cb) {
         cb(null, "public/assets")
     },
-    filename: function(req, file, cb) {
+    filename: function (req, file, cb) {
         cb(null, file.originalname)
     }
 })
@@ -51,20 +51,26 @@ app.use((req, res) => {
     res.send("Hello World")
 })
 
-
 mongoose.connect(process.env.MONGO_URL, {
     // useNewUrlParser: true, 
     // useUnifiedTopology: true
-}).then(() => {
-    app.listen(port, () => {
-        console.log(`Server is running on PORT: ${port}`);
-        
-    })
-}).catch((err) => console.log(`${err} did not`)
-)
+})
+    .then(console.log("Connection to the database is done"))
+
+
+// mongoose.connect(process.env.MONGO_URL, {
+//     // useNewUrlParser: true, 
+//     // useUnifiedTopology: true
+// }).then(() => {
+//     app.listen(port, () => {
+//         console.log(`Connection successful to the database`);
+
+//     })
+// }).catch((err) => console.log(`${err} did not`)
+// )
 
 // Actual server running 
-// app.listen(port, () => {
-//     console.log(`Server is running on PORT: ${port}`);
-    
-// })
+app.listen(port, () => {
+    console.log(`Server is running on PORT: ${port}`);
+
+})
