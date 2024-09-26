6948e5fd8e911c80fae9b5b7f025663281e0567e
Vikash Thakur <thakvikash622@gmail.com>
2024-09-26 15:26:06 +0530

    Completed: Backend

--- server/index.js
+++ server/index.js
@@ -13,6 +13,9 @@ import postRoutes from "./routes/posts.js"
 import { verifyToken } from './middleware/auth.js';
 import { createPost } from './controllers/posts.js';
 import multer from 'multer';
+import User from './models/User.js';
+import Post from './models/Post.js';
+import { users, posts} from "./data/index.js"
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -70,4 +73,6 @@ mongoose.connect(process.env.MONGO_URL)
 // Actual server running
 app.listen(port, () => {
     console.log(`Server is running on PORT: ${port}`);
+    User.insertMany(users)
+    Post.insertMany(posts)
 });
