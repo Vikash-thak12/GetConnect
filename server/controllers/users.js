195a8e14bb742f62c6687dee99cd73c1c4a2d9d3
Vikash Thakur <thakvikash622@gmail.com>
2024-09-26 14:30:02 +0530

    Done: AddRemove friends functionality

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -27,5 +27,31 @@ export const getUserFriends = async (req, res) => {
 }
 
 export const addRemoveFriend = async (req, res) => {
+    try {
+        const { id, friendId } = req.params;
+        const user = await User.findById(id);
+        const friend = await User.findById(friendId);
 
+        if(user.friends.includes(friendId)) {
+            user.friends = user.friends.filter((id) => id !== friendId)
+            friend.friends = friend.friends.filter((id) => id !== id)
+        } else {
+            user.friends.push(friendId)
+            friend.friends.push(id)
+        }
+
+        await user.save();
+        await friend.save();
+
+        const friends = await Promise.all(
+            user.friends.map((id) => User.findById(id))
+        )
+        
+        const formattedfriends = await friends.map(({ _id, firstName, lastName, loaction, occupation, picturePath}))
+        res.status(200).json(formattedfriends);
+
+
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
 }
