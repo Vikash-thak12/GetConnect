9b10af4d0b1604f20db4c86cccdb22e8227d1540
Vikash Thakur <thakvikash622@gmail.com>
2024-09-26 10:29:17 +0530

    Working: On login functionality

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -37,6 +37,38 @@ export const register = async (req, res) => {
 }
 
 
-export const login = async () => {
-    
+export const login = async (req, res) => {
+    try {
+        const {email, password} = req.body;
+        const user = await User.findOne({email: email});
+        if(!user) return res.status(400).json({ msg: "User doesn't exit"})
+        const isMatch = await bcrypt.compare(password, user.password)
+
+        if(!isMatch) return res.status(400).json({ msg: "Invalid User Credintals"})
+
+        // if (!isMatch) {
+        //     return res.status(400).json({ Message: "Invalid username and password" })
+        // } else {
+        //     return res.status(200).json({
+        //         Message: "Login Successful",
+        //         user: {
+        //             _id: user._id,
+        //             fullname: user.firstName,
+        //             email: user.email
+        //         }
+        //     }
+        //     )
+        // }
+
+        const payload = {
+            ...user
+        }
+
+        const token = jwt.sign(payload, process.env.JWT_SECRET, {expiresIn:"1hr"})
+        res.status(200).json({ user, token})
+
+
+    } catch (error) {
+        res.status(500).json({ Error: error.message})
+    }
 }
