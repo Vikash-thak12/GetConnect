e34ec5642996240ebd034256611c449134c1d2cb
Vikash Thakur <125829628+Vikash-thak12@users.noreply.github.com>
2024-09-29 22:40:21 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -38,12 +38,6 @@ app.use(morgan('common'));
 app.use(cors());
 app.use('/assets', express.static(path.join(__dirname, 'public/assets')));
 
-
-// Simple route
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
-
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
       cb(null, "public/assets");
